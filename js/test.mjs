33bcdc7a99cba1a8b4351efd3fbaaf49ce7c38e7
Ryan Lee <leeyanleryan.21@gmail.com>
2026-08-10 13:18:58 +0800

    Move js in html into its own file

--- js/test.mjs
+++ js/test.mjs
@@ -1,9 +1,34 @@
-import { loadPyodide } from "../node_modules/pyodide";
-
-async function hello_python() {
+async function main() {
   let pyodide = await loadPyodide();
-  return pyodide.runPythonAsync("1+1");
+  return pyodide;
+}
+let pyodideReadyPromise = main();
+
+async function add() {
+  let pyodide = await pyodideReadyPromise;
+  try {
+    pyodide.runPython(`
+    import js
+    inp1 = js.document.getElementById("test1")
+    inp2 = js.document.getElementById("test2")
+    total = int(inp1.value) + int(inp2.value)
+    js.document.getElementById("output").innerHTML = total
+  `);
+  } catch(err) {
+    document.getElementById("output").innerHTML = "Error!"
+  }
 }
 
-const result = await hello_python();
-console.log("Python says that 1+1 =", result);
+document.getElementById("add-button").addEventListener("click", add);
+
+async function multiplyby2() {
+  let pyodide = await pyodideReadyPromise;
+  try {
+    pyodide.globals.set("total", pyodide.globals.get("total")*2);
+    document.getElementById("output").innerHTML = pyodide.globals.get("total");
+  } catch(err) {
+    document.getElementById("output").innerHTML = "Error!"
+  }
+}
+
+document.getElementById("multiply-button").addEventListener("click", multiplyby2);
