3da38bf0c12a16dba0937ce9df33d778b48e8bef
Ryan Lee <leeyanleryan.21@gmail.com>
2026-08-10 14:29:48 +0800

    Separate python code from js

--- js/test.mjs
+++ js/test.mjs
@@ -1,20 +1,23 @@
 async function main() {
   let pyodide = await loadPyodide();
+  const testpy = await fetch("./py/test.py");
+  const testpyCode = await testpy.text();
+  await pyodide.runPythonAsync(testpyCode);
   return pyodide;
 }
+
 let pyodideReadyPromise = main();
 
 async function add() {
   let pyodide = await pyodideReadyPromise;
   try {
-    pyodide.runPython(`
-    import js
-    inp1 = js.document.getElementById("test1")
-    inp2 = js.document.getElementById("test2")
-    total = int(inp1.value) + int(inp2.value)
-    js.document.getElementById("output").innerHTML = total
-  `);
+    const inp1 = document.getElementById("test1").value;
+    const inp2 = document.getElementById("test2").value;
+    const pythonAdd = pyodide.globals.get("add");
+    const total = pythonAdd(inp1, inp2);
+    document.getElementById("output").innerHTML = total;
   } catch(err) {
+    console.error(err);
     document.getElementById("output").innerHTML = "Error!"
   }
 }
@@ -24,9 +27,12 @@ document.getElementById("add-button").addEventListener("click", add);
 async function multiplyby2() {
   let pyodide = await pyodideReadyPromise;
   try {
-    pyodide.globals.set("total", pyodide.globals.get("total")*2);
-    document.getElementById("output").innerHTML = pyodide.globals.get("total");
+    const total = Number(document.getElementById("output").innerHTML);
+    const pythonMultiplyBy2 = pyodide.globals.get("multiply_by_2");
+    const result = pythonMultiplyBy2(total);
+    document.getElementById("output").innerHTML = result;
   } catch(err) {
+    console.error(err);
     document.getElementById("output").innerHTML = "Error!"
   }
 }
---
+++ py/test.py
@@ -0,0 +1,5 @@
+def add(a, b):
+  return int(a) + int(b)
+
+def multiply_by_2(val):
+  return int(val) * 2
